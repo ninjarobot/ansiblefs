d833058e8e1a931d4ab0a1d07baeef558c516322
Dave Curylo <curylod@asme.org>
2019-04-27 15:16:06 -0400

    Working Ansible module with F# & CoreRT.

--- Makefile
+++ Makefile
@@ -10,6 +10,12 @@ restore:
 build: restore
 	dotnet build
 
+native-linux:
+	dotnet publish -r linux-x64 -c Release
+
+native-macos:
+	dotnet publish -r osx-x64 -c Release
+
 test: build
 	dotnet test
 
--- src/anisiblefs/Program.fs
+++ src/anisiblefs/Program.fs
@@ -1,8 +1,57 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System.IO
+open Chiron
+open Chiron.Operators
 
-open System
+type ModuleArgs = {
+    Name : string
+}
+with
+    static member create name =
+        {
+            Name = name
+        }
+    static member FromJson(_:ModuleArgs) =
+        ModuleArgs.create
+        <!> Json.readOrDefault "name" "World"
+
+type Response = {
+    Message : string
+    Changed : bool
+    Failed : bool
+}
+with
+    static member ToJson (r:Response) =
+           Json.write "msg" r.Message
+        *> Json.write "changed" r.Changed
+        *> Json.write "failed" r.Failed
+
+module Result =
+    let ofException fn =
+        try
+            let r = fn
+            r |> Result.Ok
+        with
+            | ex -> ex |> Result.Error
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    match argv with
+    | [|argsFilePath|] ->
+        match File.ReadAllText argsFilePath |> Result.ofException with
+        | Result.Ok(json) ->
+            let (args:ModuleArgs) = json |> Json.parse |> Json.deserialize
+            let result = { Message = (System.String.Concat ("Hello, ", args.Name, " from F# module.")); Changed = true; Failed = false }
+            System.Console.WriteLine(result |> Json.serialize |> Json.format)
+        | Result.Error(ex) ->
+            let result = { Message = (System.String.Concat ("Error reading arguments file: ", ex)); Changed = true; Failed = true }
+            System.Console.WriteLine(result |> Json.serialize |> Json.format)
+    | _ ->
+        let result =
+            {
+                Message = "Module expects a single argument (args file path)."
+                Changed = false
+                Failed = true
+            }
+        System.Console.WriteLine(result |> Json.serialize |> Json.format)
+    
+    0
